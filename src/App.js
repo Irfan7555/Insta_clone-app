d89f98921c93ecae28f74aa32a3cc94509bf55c5
Irfan <irfan93940@gmail.com>
2024-02-23 12:21:41 +0530

    Read post from fastapi and display in console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
+const BASE_URL = 'http://localhost:8000/'
+
 function App() {
+
+  const [posts, setPosts]= useState([]);
+
+  useEffect(() =>{
+      fetch(BASE_URL + 'post/all')
+        .then(response => {
+          console.log(response.json());
+          if (response.ok){
+            return response.json()
+          }
+          throw response
+        })
+        .then(data =>{
+          setPosts(data)
+        })
+        .catch(error =>{
+          console.log(error);
+          alert(error)
+        })
+  }, [])
+
   return (
     "Hello Irfan What do you do"
   );
