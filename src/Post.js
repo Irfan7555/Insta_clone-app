6a770ae82073a86cc087438b73775128eea8a372
Irfan <irfan93940@gmail.com>
2024-02-23 17:40:57 +0530

    Adding Post Header

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,7 @@
 import React,{useState, useEffect} from "react";
 import './Post.css'
+import {Avatar, Button} from "@mui/material";
+
 
 const BASE_URL = 'http://localhost:8000/'
 
@@ -22,6 +24,13 @@ function Post({ post }){
 
     return (
         <div className="post">
+            <div className="post_header">
+                <Avatar alt="Irfan Shareef" src="https://easydrawingguides.com/wp-content/uploads/2022/08/easy-cat-face-11.png"></Avatar>
+                <div className="post_headerInfo">
+                    <h3>{post.user.username}</h3>
+                    <Button className="post_delete">DELETE</Button>
+                </div>
+            </div>
             <img className="post_image" src={imageUrl}/>
             <h4 className="post_text">{ post.caption }</h4>
             <div className="post_comments">
